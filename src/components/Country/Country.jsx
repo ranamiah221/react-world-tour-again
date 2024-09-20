0fe073384e2fc95d61d326d66cd233a0e88ae266
ranamiah221 <ranarasul21@gmail.com>
2024-09-20 20:50:28 +0600

    added population and region and remove react prototype

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,11 +2,13 @@
 
 const Country = ({country}) => {
     console.log(country)
-    const {name, flags}=country;
+    const {name, flags, population, region}=country;
     return (
         <div style={{border:"2px solid skyblue", margin:'15px',padding:'15px', borderRadius:'15px'}}>
             <h3>name : {name?.common}</h3>
             <img src={flags?.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Region: {region}</p>
         </div>
     );
 };
