d36695573b466b04be4c3e38a61ff46764cfeb7b
ranamiah221 <ranarasul21@gmail.com>
2024-09-20 21:49:09 +0600

    added event handler and create visited country list using mark visited button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 
 
-const Country = ({country}) => {
-    console.log(country)
+const Country = ({country,handleVisited}) => {
+
     const {name, flags, population, region}=country;
     return (
         <div style={{border:"2px solid skyblue", margin:'15px',padding:'15px', borderRadius:'15px'}}>
@@ -9,6 +9,7 @@ const Country = ({country}) => {
             <img src={flags?.png} alt="" />
             <p>Population: {population}</p>
             <p>Region: {region}</p>
+            <button onClick={()=>handleVisited(country)} style={{background:'skyblue'}}>Mark Visited</button>
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,21 +1,40 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
-
+import "./Countries.css"
 
 const Countries = () => {
     const [countries, setCountries]=useState([]);
+    const [visitedCountry, setVisitedCountry]=useState([]);
+    const handleVisited=(country)=>{
+        const newVisitedCountry=[...visitedCountry, country];
+        setVisitedCountry(newVisitedCountry);
+    }
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data=>setCountries(data))
     },[])
+
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h2>Visited Country : {visitedCountry.length}</h2>
+                <ol>
+                    {
+                        visitedCountry.map(country=> <li key={country.cca3}>{country.name?.common}</li>)
+                    }
+                </ol>
+
+            </div>
+            <div className="country-container">
             {
-                countries.map(country=><Country key={country.cca3} country={country}></Country>)
+                countries.map(country=><Country key={country.cca3}
+                     country={country}
+                     handleVisited={handleVisited}></Country>)
             }
+            </div>
         </div>
     );
 };
